55ed3cc84cc612af9b7f87d726daf86f84019979
Mincho Park <s2018s28@e-mirim.hs.kr>
2020-05-19 11:02:12 +0900

    초기 커밋.

---
+++ StudentList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    class Program
+    {
+        class Teacher
+        {
+            public string name;
+            public string subject;
+        }
+        static void Main(string[] args)
+        {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() { name = "함기훈", subject = "뉴미디어콘텐츠제작" });
+            list.Add(new Teacher() { name = "박민초", subject = "뉴미디어콘텐츠제작" });
+        }
+    }
+}
